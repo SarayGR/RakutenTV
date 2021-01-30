8fea8ed26329b69f02dd193c52001fc0e186d731
SarayGR <saraygromero@gmail.com>
2021-01-30 20:02:56 +0100

    The Movie DB - 001 Splash
    Why:
    * Implement Splash screen.
    How:
    * XML and Logic

--- app/src/main/java/com/example/rakutentv/SplashActivity.java
+++ app/src/main/java/com/example/rakutentv/SplashActivity.java
@@ -1,16 +1,33 @@
 package com.example.rakutentv;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int DURATION_SPLASH = 5000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURATION_SPLASH);
 
     }
 }
